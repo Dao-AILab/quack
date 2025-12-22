83de1089ecee188cd7782d08de396f1d434477ff
Tri Dao <tridpq@gmail.com>
2025-12-22 16:05:09 +0900

    [Compile] make_fake_tensor now supports passing in leading_dim

--- quack/compile_utils.py
+++ quack/compile_utils.py
@@ -5,12 +5,15 @@ from typing import Optional
 import cutlass.cute as cute
 
 
-def make_fake_tensor(dtype, shape, divisibility=1) -> Optional[cute.Tensor]:
+def make_fake_tensor(dtype, shape, divisibility=1, leading_dim=-1) -> Optional[cute.Tensor]:
+    if leading_dim < 0:
+        leading_dim = len(shape) + leading_dim
     if dtype is None:
         return None
+    stride = tuple(
+        cute.sym_int64(divisibility=divisibility) if i != leading_dim else 1
+        for i in range(len(shape))
+    )
     return cute.runtime.make_fake_tensor(
-        dtype,
-        shape,
-        stride=(*[cute.sym_int64(divisibility=divisibility)] * (len(shape) - 1), 1),
-        assumed_align=divisibility * dtype.width // 8,
+        dtype, shape, stride=stride, assumed_align=divisibility * dtype.width // 8
     )
